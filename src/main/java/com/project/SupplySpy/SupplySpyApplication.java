884a61763e68a67d48e2e0a7a595d12c13fbb1dd
Aldo Grabić <aldo.grabic99@gmail.com>
2024-02-27 20:54:53 +0100

    Prvi commit

---
+++ src/main/java/com/project/SupplySpy/SupplySpyApplication.java
@@ -0,0 +1,13 @@
+package com.project.SupplySpy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplySpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplySpyApplication.class, args);
+	}
+
+}
